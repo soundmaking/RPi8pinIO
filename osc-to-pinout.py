3969f645ad151967a5af709227e7bfc4211511ce
sdf <soundmaking@merfasmean.com>
2014-10-14 17:28:43 +0100

    adding osc-to-pinout.py (in dev)

---
+++ osc-to-pinout.py
@@ -0,0 +1,37 @@
+import argparse
+import math
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def print_volume_handler(unused_addr, args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
+
+def print_compute_handler(unused_addr, args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+def pinout_1_handler(unused_addr, args, value):
+	print("[{0}] ~ {1}".format(args[0], value))
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+      default="127.0.0.1", help="The ip to listen on")
+  parser.add_argument("--port",
+      type=int, default=5005, help="The port to listen on")
+  args = parser.parse_args()
+
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/debug", print)
+  dispatcher.map("/volume", print_volume_handler, "Volume")
+  dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+  dispatcher.map("/pinout1", pinout_1_handler, "PinOut_1")
+
+  server = osc_server.ThreadingOSCUDPServer(
+      (args.ip, args.port), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
+
