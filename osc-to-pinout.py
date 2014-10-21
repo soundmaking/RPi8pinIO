a0a347f8f4a670edf8addc1e15e66c5c5adc702d
sdf <soundmaking@merfasmean.com>
2014-10-21 17:38:09 +0100

    added some code in comments to be tested later on the RPi

--- osc-to-pinout.py
+++ osc-to-pinout.py
@@ -1,8 +1,14 @@
 """ python3
-	coding
-	by sdf
+	coding by sdf
 	soundmaking.co.uk
 """
+
+""" code marked 'added but not tested' 
+	should be uncommented hen ready to test it...
+	... other lines may beed to be commented out 
+	when testing the new ones!
+"""
+
 devmode = True
 
 import RPi.GPIO as GPIO
@@ -22,6 +28,15 @@ GPIO.setmode(GPIO.BOARD)
 # 3p _3 _5 _7 Gd 11 13 15 3p 19 21 24 Gd #
 # -------------------------------------- #
 
+""" added but not tested 1 of 3 (changed pin out mapping)
+
+#           8  7  6     5  4     3  2  1 # 
+#  |  |  |  |  |  |  |  |  |  |  |  |  | #
+# 5v 3p Gd _8 10 12 Gd 16 18 Gd 22 24 26 #
+
+outpin = (26, 24, 22, 18, 16, 12, 10, 8)
+
+# end of added but not tested 1 of 3 : comment below to test above """
 
 #           1  2  3     4  5     6  7  8 # 
 #  |  |  |  |  |  |  |  |  |  |  |  |  | #
@@ -40,6 +55,7 @@ def set_relay(pin_, val_):
     v = 0
   GPIO.output(outpin[pin_-1], v)
 
+
 def pinout_1_handler(unused_addr, args, value):
 	set_relay(1, value)
 
@@ -51,6 +67,24 @@ def pinout_3_handler(unused_addr, args, value):
 
 def pinout_4_handler(unused_addr, args, value):
 	set_relay(4, value)
+
+
+
+""" # added but not tested 2 of 3 
+def pinout_5_handler(unused_addr, args, value):
+	set_relay(5, value)
+
+def pinout_6_handler(unused_addr, args, value):
+	set_relay(6, value)
+
+def pinout_7_handler(unused_addr, args, value):
+	set_relay(7, value)
+
+def pinout_8_handler(unused_addr, args, value):
+	set_relay(8, value)
+
+# end of added but not tested 2 of 3  --- test at same time as 3 of 3 """
+
 
 
 def kill_it(unused_addr, args, val):
@@ -77,6 +111,14 @@ if __name__ == "__main__":
   dispatcher.map("/pinout2", pinout_2_handler, "PinOut_2")
   dispatcher.map("/pinout3", pinout_3_handler, "PinOut_3")
   dispatcher.map("/pinout4", pinout_4_handler, "PinOut_4")
+
+  """ # added but not tested 3 of 3 
+  dispatcher.map("/pinout5", pinout_5_handler, "PinOut_5")
+  dispatcher.map("/pinout6", pinout_6_handler, "PinOut_6")
+  dispatcher.map("/pinout7", pinout_7_handler, "PinOut_7")
+  dispatcher.map("/pinout8", pinout_8_handler, "PinOut_8")
+  
+  # end of added but not tested 3 of 3 """ 
 
 
   dispatcher.map("/killit", kill_it, "Kill")
